3f7bf35f0b2b3ca5b404ef5c38a7a543dd1033c3
sithlii <isaac.gartz@gmail.com>
2025-03-21 15:06:35 -0600

    class component assign.

---
+++ react/class-components/src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <h1>World</h1>
+      </div>
+    )
+  }
+}
+export default App;
